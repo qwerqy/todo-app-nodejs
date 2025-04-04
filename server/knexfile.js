299ca0f437c391db9f675a6c601d0fbb0a121e72
Amin Roslan <amnrsln@gmail.com>
2025-04-04 19:25:08 +0800

    fix: knex and deps

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,67 +4,70 @@
   Knex also allows for easy switching between databases. 
   But the .returning() method will only work for PostgreSQL, MSSQL, and Oracle databases.
 */
-require('dotenv').config();
+require('dotenv').config()
+
 module.exports = {
+	test: {
+		client: 'postgresql',
+		connection: {
+			database: process.env.PGDATABASE,
+			user: process.env.PGUSER,
+			password: process.env.PGPASSWORD,
+			host: process.env.PGHOST,
+			port: process.env.PGPORT,
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: 'knex_migrations',
+		},
+	},
 
-  test: {
-    client: 'postgresql',
-    connection: {
-      database: process.env.PGDATABASE,
-      user:     process.env.PGUSER,
-      password: process.env.PGPASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+	development: {
+		client: 'postgresql',
+		connection: {
+			database: process.env.PGDATABASE,
+			user: process.env.PGUSER,
+			password: process.env.PGPASSWORD,
+			host: process.env.PGHOST,
+			port: process.env.PGPORT,
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: 'knex_migrations',
+		},
+	},
 
-  development: {
-    client: 'postgresql',
-    connection: {
-      database: process.env.PGDATABASE,
-      user:     process.env.PGUSER,
-      password: process.env.PGPASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+	staging: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password',
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: 'knex_migrations',
+		},
+	},
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+	production: {
+		client: 'postgresql',
+		connection: process.env.DATABASE_URL,
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: 'knex_migrations',
+		},
+	},
+}
